44c92d5895af53cec372431029d0d9b63015a20f
Ethan Hanlon <me@ethan-hanlon.xyz>
2022-12-23 20:44:48 -0800

    Changes 12/23

---
+++ copilot.py
@@ -0,0 +1,8 @@
+# Return the quotient of two numbers
+def main():
+    print("This program finds the quotient of two numbers.")
+    x = int(input("Enter the numerator: "))
+    y = int(input("Enter the denominator: "))
+    print(x, "divided by", y, "is", x/y)
+
+main()
